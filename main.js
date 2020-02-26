c9813112aa8deb93b7c6af7abdd85c61fea94aa8
JillMOry <oryjill@gmail.com>
2020-02-25 19:44:44 -0600

    with added catch error statement

--- main.js
+++ main.js
@@ -7,11 +7,16 @@ window.onload = () => {
 
 // this function is going to make a fetch request to the url inside it's parameter brackets (). Then it will turn the response (data it's getting back), saved here as res. The res.json will not be saved as posts and saved into the variable, arrayOfPosts
 const getPosts = () => {
-	fetch("https://randomuser.me/api/?results=10")
+	fetch("https://randomuser.me/api/?results=12")
 		.then((res) => res.json())
 		.then((users) => {
 			arrayOfPosts = users.results;
-		});
+			//displays posts when page loads
+			displayPost();
+		})
+		//added to fetch error
+		.then((posts) => (arrayOfPosts = posts))
+		.catch((err) => console.log(`Error,  ${err}`));
 };
 
 // this function logs the results in your browsers console
@@ -23,18 +28,26 @@ const displayPost = () => {
 	const allPosts = document.getElementById("Contacts");
 	arrayOfPosts.map((user) => {
 		console.log(arrayOfPosts[0]);
+		// create a list element
 		const li = document.createElement("li");
+		// create an image element
 		const showImg = document.createElement("img");
+		// display pic with .src this is essentially <img src="url">
 		showImg.src = user.picture.thumbnail;
+		// creates the text area for the first and last name.
 		const text = document.createTextNode(
 			`${user.name.last}, ${user.name.first}`
 		);
+		//append inserts the specified content
 		allPosts.append(li);
 		li.appendChild(showImg);
 		li.appendChild(text);
 
+		//creates HTML button
 		const button = document.createElement("button");
+		//"names" the button
 		button.innerHTML = "more info";
+		//onclick adds the additional contact information
 		button.onclick = function() {
 			const text2 = document.createTextNode(
 				`  ${user.cell}, ${user.phone}, ${(user.location.street.number,
@@ -42,10 +55,10 @@ const displayPost = () => {
 					user.location.state
 				}, ${user.location.postcode}`
 			);
+			//append inserts the specified content
 			li.appendChild(text2);
 		};
+		//append inserts the specified content
 		li.appendChild(button);
 	});
 };
-
-//
